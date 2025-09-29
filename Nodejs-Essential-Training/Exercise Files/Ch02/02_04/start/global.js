c01774c53e2690e04879e202fd3d3912545fca5e
violetacf <violeta@cique.es>
2025-09-29 08:47:03 +0200

    Exercise 02_04

--- Nodejs-Essential-Training/Exercise Files/Ch02/02_04/start/global.js
+++ Nodejs-Essential-Training/Exercise Files/Ch02/02_04/start/global.js
@@ -1,10 +1,43 @@
-function grab(flag) {
-  let indexAfterFlag = process.argv.indexOf(flag) + 1;
-  return process.argv[indexAfterFlag];
+// function grab(flag) {
+//   let indexAfterFlag = process.argv.indexOf(flag) + 1;
+//   return process.argv[indexAfterFlag];
+// }
+
+// let greeting = grab("--greeting");
+// let user = grab("--user");
+
+// console.log(greeting);
+// console.log(user);
+
+process.stdout.write("Hello \n \n");
+
+const questions = [
+  "What is your name? ",
+  "What is your favorite hobby? ",
+  "What is your preferred programming language? ",
+];
+
+const answers = [];
+
+function ask(i = 0) {
+  process.stdout.write(`\n\n ${questions[i]}`);
+  process.stdout.write(" > ");
 }
 
-let greeting = grab("--greeting");
-let user = grab("--user");
+ask();
 
-console.log(greeting);
-console.log(user);
+process.stdin.on("data", function (data) {
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on("exit", function () {
+  process.stdout.write("\n\n");
+  process.stdout.write(
+    `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later! \n`
+  );
+});
